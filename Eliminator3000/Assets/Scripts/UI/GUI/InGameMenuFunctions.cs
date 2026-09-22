ade92215d9947e4d0fb9af0ffd8fe66f21879e46
Freek Michielsen <freek.mich@gmail.com>
2026-09-22 15:48:51 +0200

    Add: return button on win or lose

---
+++ Eliminator3000/Assets/Scripts/UI/GUI/InGameMenuFunctions.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class InGameMenuFunctions : MonoBehaviour
+{
+    [SerializeField] private GameObject inGameMenu;
+
+    private void OnEnable()
+    {
+        EventBus<GameWinEvent>.Subscribe(OnGameWinEvent);
+        EventBus<GameLoseEvent>.Subscribe(OnGameLoseEven);
+    }
+
+    private void OnDestroy()
+    {
+        EventBus<GameWinEvent>.UnSubscribe(OnGameWinEvent);
+        EventBus<GameLoseEvent>.UnSubscribe(OnGameLoseEven);
+    }
+
+    private void Start()
+    {
+        inGameMenu.SetActive(false);
+    }
+
+    public void OnMainMenuButtonPressed()
+    {
+        EventBus<LoadSceneEvent>.Publish(new LoadSceneEvent("MenuScene"));
+    }
+
+    private void EnableMenu()
+    {
+        inGameMenu.SetActive(true);
+    }
+
+    private void OnGameWinEvent(GameWinEvent _winEvent)
+    {
+        EnableMenu();
+    }
+
+    private void OnGameLoseEven(GameLoseEvent _loseEvent)
+    {
+        EnableMenu();
+    }
+}
