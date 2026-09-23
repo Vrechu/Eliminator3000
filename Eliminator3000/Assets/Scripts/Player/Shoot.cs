6732857675b791ff6411b7b94d66049c65f4a22e
Freek Michielsen <freek.mich@gmail.com>
2026-09-23 12:48:03 +0200

    Add: new projectile attack
    - added a new projectile to be shot after pickup
    - reorganized the prefab folder

--- Eliminator3000/Assets/Scripts/Player/Shoot.cs
+++ Eliminator3000/Assets/Scripts/Player/Shoot.cs
@@ -5,12 +5,15 @@ using UnityEngine.InputSystem.XR;
 public class Shoot : MonoBehaviour
 {
     [SerializeField]
-    private GameObject projectilePrefab;
+    private GameObject projectilePrefab, upgradeProjectilePrefab;
     private GameStateManager gameStateManager;
+    private SetGunType setGunType;
+
 
     private void Start()
     {
         gameStateManager = GameStateManager.Instance;
+        setGunType = GetComponent<SetGunType>();
     }
 
     public void GetShootInput(InputAction.CallbackContext _context)
@@ -19,11 +22,31 @@ public class Shoot : MonoBehaviour
         if (gameStateManager.CurrentState 
             != GameStateManager.GameState.Ingame) return;
         if (!_context.performed) return;
-        ShootProjectile();
+        CheckWeaponType();
     }
 
-    private void ShootProjectile()
+    private void ShootRegularProjectile()
     {
         Instantiate(projectilePrefab, transform.position, Quaternion.Euler(90f,0f,0f));
+    }
+    private void ShootUpgradeProjectile()
+    {
+        Instantiate(upgradeProjectilePrefab, transform.position, Quaternion.Euler(90f, 0f, 0f));
+    }
+
+    private void CheckWeaponType()
+    {
+        switch (setGunType.gunType)
+        {
+            case SetGunType.GunType.Base:
+                ShootRegularProjectile();
+                break;
+            case SetGunType.GunType.Gun:
+                ShootUpgradeProjectile();
+                break;
+            default:
+                Debug.LogWarning("Unknown gun type.");
+                break;
+        }
     }
 }
